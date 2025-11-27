7cccc4ff4552ea506b2f86f8d034fb21950c02f5
henryflia <114320959+Henryflia@users.noreply.github.com>
2025-11-26 21:18:55 -0400

    Add files via upload

---
+++ src/js/ProductDetails.mjs
@@ -0,0 +1,63 @@
+import { getLocalStorage, setLocalStorage, alertMessage} from "./utils.mjs";
+
+
+
+export default class ProductDetails{
+    constructor(productId, dataSource){
+        this.productId = productId;
+        this.product = {};
+        this.dataSource = dataSource;
+    }
+    async init() {
+        // use the datasource to get the details for the current product. findProductById will return a promise! use await or .then() to process it
+        this.product = await this.dataSource.findProductById(this.productId)
+      // the product details are needed before rendering the HTML
+      console.log(this.product.Colors)
+      this.renderProductDetails();
+      this.renderColorOptions(this.product.Colors);
+        // once the HTML is rendered, add a listener to the Add to Cart button
+        document
+            .getElementById("add-to-cart")
+            .addEventListener("click", this.addProductToCart.bind(this));
+// Notice the .bind(this). This callback will not work if the bind(this) is missing. Review the readings from this week on 'this' to understand why.
+    }
+    addProductToCart() {
+        const cartItems = getLocalStorage("so-cart") || [];
+        cartItems.push(this.product);
+        setLocalStorage("so-cart", cartItems);
+        alertMessage(`${this.product.NameWithoutBrand} added to cart!`);
+    }
+    
+  renderProductDetails() {
+      productDetailsTemplate(this.product);
+  }
+  
+  renderColorOptions(colors) {
+    const container = document.querySelector(".color-options")
+    container.innerHTML = colors.map(color =>
+      `<label class="color-option">
+        <input type="checkbox" name="colors" value"${color.ColorCode}">
+        <img src="${color.ColorChipImageSrc}" alt="$"{color.ColorName}">
+        <span>${color.ColorName}</span>
+      </label>`
+    ).join("")
+  }
+}
+function productDetailsTemplate(product) {
+  document.querySelector("h2").textContent = product.Category.charAt(0).toUpperCase() + product.Category.slice(1);
+  document.querySelector("#p-brand").textContent = product.Brand.Name;
+  document.querySelector("#p-name").textContent = product.NameWithoutBrand;
+
+  const productImage = document.querySelector("#p-image");
+  productImage.src = product.Images.PrimaryExtraLarge;
+  productImage.alt = product.NameWithoutBrand;
+  const euroPrice = new Intl.NumberFormat("de-DE",
+    {
+      style: "currency", currency: "EUR",
+    }).format(Number(product.FinalPrice) * 0.85);
+  document.querySelector("#p-price").textContent = `${euroPrice}`;
+  document.querySelector("#p-color").textContent = product.Colors[0].ColorName;
+  document.querySelector("#p-description").innerHTML = product.DescriptionHtmlSimple;
+
+  document.querySelector("#add-to-cart").dataset.id = product.Id;
+}
---
+++ src/js/cart.js
@@ -0,0 +1,6 @@
+import ShoppingCart from "./ShoppingCart.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
+
+const cart = new ShoppingCart("so-cart", ".product-list");
+loadHeaderFooter();
+cart.renderCartContents();
