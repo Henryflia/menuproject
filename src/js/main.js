ca4d3a2ef9b0c296e398ba48eb2afa20cd763708
henryflia <superflia2059@gmail.com>
2025-11-29 21:07:20 -0400

    W5 part 1

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,13 @@
 import { loadHeaderFooter } from "./utils.mjs";
 
 loadHeaderFooter();
+
+const grid = document.getElementById("product-grid");
+
+document.getElementById("left-btn").addEventListener("click", () => {
+  grid.scrollBy({ left: -200, behavior: "smooth" });
+});
+
+document.getElementById("right-btn").addEventListener("click", () => {
+  grid.scrollBy({ left: 200, behavior: "smooth" });
+});
--- vite.config.js
+++ vite.config.js
@@ -10,9 +10,10 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "src/index.html"),
         cart: resolve(__dirname, "src/cart/index.html"),
+        save: resolve(__dirname, "src/save/index.html"),
         checkout: resolve(__dirname, "src/checkout/index.html"),
-        product: resolve(__dirname, "src/product_pages/index.html"),
-        productlisting: resolve(__dirname, "src/product_listing/index.html"),
+        product: resolve(__dirname, "src/food_pages/index.html"),
+        productlisting: resolve(__dirname, "src/food_listing/index.html"),
       },
     },
   },
